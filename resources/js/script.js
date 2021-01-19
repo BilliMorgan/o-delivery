b7d6ef7f1d71e043991bcdde623ea02bff9f9361
Andrii Shymko <55424136+BilliMorgan@users.noreply.github.com>
2021-01-18 21:10:24 -0500

    responsive design is done

--- resources/js/script.js
+++ resources/js/script.js
@@ -64,10 +64,50 @@ $(document).ready(function () {
       }
     });
 
-    // Animation on scroll
-    $(".js--wp-1").waypoint(function(direction){
-      $(".js--wp-1").addClass('animated fadeIn');
-    }, {
-      offset: '50%'
-    });
+  // Animation on scroll
+  $(".js--wp-1").waypoint(
+    function (direction) {
+      $(".js--wp-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-2").waypoint(
+    function (direction) {
+      $(".js--wp-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-3").waypoint(
+    function (direction) {
+      $(".js--wp-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  $(".js--wp-4").waypoint(
+    function (direction) {
+      $(".js--wp-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%",
+    }
+  );
+
+  //Mobile navigation
+  $(".js--nav-icon").click(function() {
+    let nav = $(".js--main-nav")
+    let icon = $(".js--nav-icon ion-icon");
+    
+    nav.slideToggle(200);
+
+
+  });
 });
